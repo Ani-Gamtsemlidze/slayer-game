acc3ecbef0b31b12e24013bc6afaec31daa5edcc
User-s <ani.gamcemlidze20@gmail.com>
2023-06-18 17:56:38 +0400

    points-add

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ heal = document.querySelector("#heal");
 
 surrender = document.querySelector("#give-up");
 
+let gameStart = true;
+
 let monsterHealth = 100;
 let yourHealth = 100;
 
@@ -15,14 +17,20 @@ let attackCount = 0;
 
 let healing = document.querySelector(".healing-log");
 
-let playerLiText;
-let monsterLiText;
-
 let attackClick = 0;
 
 let monsterPoint = document.getElementById("monster-point");
 let playerPoint = document.getElementById("player-point");
 
+let monsterPoints = 0;
+let playerPoints = 0;
+
+let playerLiText;
+let monsterLiText;
+
+let monsterWon = document.querySelector(".monster-winner");
+let playerWon = document.querySelector(".player-winner");
+// ATTACK button function
 attackButton.addEventListener("click", function () {
   attackClick++;
 
@@ -38,35 +46,45 @@ attackButton.addEventListener("click", function () {
   monster.style.width = `${monsterHealth}%`;
   player.style.width = `${yourHealth}%`;
 
-  let playerLiText = document.createElement("li");
-  let monsterLiText = document.createElement("li");
+  playerLiText = document.createElement("li");
+  monsterLiText = document.createElement("li");
 
   document.querySelector(".list").appendChild(playerLiText);
   document.querySelector(".list").appendChild(monsterLiText);
 
   const list = document.querySelector(".list");
 
-  // insert before
   list.insertBefore(playerLiText, list.firstChild);
   list.insertBefore(monsterLiText, list.firstChild);
+  // insert before
+
+  // monster texts
 
   monsterLiText.innerHTML = `MONSTER HIT THE PLAYER FOR ${attackPlayerNum} DAMAGE`;
 
-  let damageValue = parseInt(monsterLiText.innerHTML.split(" ")[5]);
-
-  // let monsterSum = damageValue + damageValue;
-  monsterPoint.innerHTML = damageValue;
-  // console.log(damageValue + damageValue);
-
   monsterLiText.classList.add("demo");
+  playerLiText.classList.add("demo");
+
+  surrender.addEventListener("click", function () {
+    playerLiText.remove();
+    monsterLiText.remove();
+  });
+
+  // if (monsterHealth < 0) {
+  //   monsterHealth = 0;
+  //   playerLiText.remove();
+  //   monsterLiText.remove();
+  // }
+
+  monsterWon.style.display = "none";
+  playerWon.style.display = "none";
 
   monsterLiText.style.color = `#d02a2a`;
   monsterLiText.style.fontSize = `20px`;
 
-  let playerSpan = attackNum;
+  // player texts
 
-  playerLiText.innerHTML = `PLAYER HIT THE MONSTER FOR ${playerSpan} DAMAGE`;
-  playerPoint.innerHTML = playerSpan + playerSpan;
+  playerLiText.innerHTML = `PLAYER HIT THE MONSTER FOR ${attackNum} DAMAGE`;
   playerLiText.classList.add("demo");
   playerLiText.style.color = `#00a876`;
 
@@ -75,6 +93,7 @@ attackButton.addEventListener("click", function () {
   console.log(`Attack button clicked ${attackClick} times.`);
 });
 
+// SPECIAL attak function
 specialAttackButton.addEventListener("click", function () {
   if (attackCount < 1) {
     let attackNum = Math.trunc(Math.random() * 40) + 1;
@@ -89,7 +108,30 @@ specialAttackButton.addEventListener("click", function () {
 
     monster.style.width = `${monsterHealth}%`;
     player.style.width = `${yourHealth}%`;
+
+    // player li creation on special attack
+
+    let playerLiText = document.createElement("li");
+    let monsterLiText = document.createElement("li");
+
+    document.querySelector(".list").appendChild(playerLiText);
+    document.querySelector(".list").appendChild(monsterLiText);
+
+    const list = document.querySelector(".list");
+
+    monsterLiText.innerHTML = `MONSTER HIT THE PLAYER FOR ${attackPlayerNum} DAMAGE`;
+    monsterLiText.classList.add("demo");
+    monsterLiText.style.color = `#d02a2a`;
+    monsterLiText.style.fontSize = `20px`;
+
+    playerLiText.innerHTML = `PLAYER HIT THE MONSTER FOR ${attackNum} DAMAGE`;
+    playerLiText.classList.add("demo");
+    playerLiText.style.color = `#00a876`;
+
+    playerLiText.style.fontSize = `20px`;
   }
+  playerWon.style.display = "none";
+
   attackCount++;
   specialAttackButton.disabled = true;
 
@@ -97,6 +139,7 @@ specialAttackButton.addEventListener("click", function () {
   //   clickCounting();
 });
 
+// healing button function
 let healingCount = 0;
 
 heal.addEventListener("click", function () {
@@ -109,35 +152,46 @@ heal.addEventListener("click", function () {
       yourHealth = 100;
     }
     player.style.width = `${yourHealth}%`;
+
+    let playerLiText = document.createElement("li");
+    document.querySelector(".list").appendChild(playerLiText);
+
+    const list = document.querySelector(".list");
+    playerLiText.innerHTML = `PLAYER HEALS FOR ${attackNum} times`;
+    playerLiText.style.color = `#00a876`;
+
+    playerLiText.style.fontSize = `20px`;
+    list.insertBefore(playerLiText, list.firstChild);
   }
   heal.disabled = true;
   healingCount++;
   healing.innerHTML = `PLAYER GAINS ${yourHealth} HP`;
-  // document.querySelector("healing-log").style.display = `block`;
 });
+
+// giving up function
 
 surrender.addEventListener("click", function () {
   const confirmed = confirm("GIVE UP?");
   if (confirmed) {
-    location.reload();
+    // location.reload();
+    playerPoints = 0;
+    playerPoint.innerHTML = playerPoints;
+    monsterPoints = 0;
+    monsterPoint.innerHTML = monsterPoints;
     startAgain();
     // playerLiText.removeChild();
     // monsterLiText.removeChild();
   }
 });
 
+// reset game
 function startAgain() {
-  location.reload();
-
   attackClick = 0;
   monsterHealth = 100;
   yourHealth = 100;
 
   monster.style.width = `${monsterHealth}%`;
   player.style.width = `${yourHealth}%`;
-
-  // monsterText.innerHTML = ` `;
-  // playerText.innerHTML = ``;
 
   attackCount = 0;
   healingCount = 0;
@@ -152,16 +206,14 @@ function startAgain() {
   attackButton.disabled = false;
   specialAttackButton.disabled = false;
   heal.disabled = false;
-
-  // healing.innerHTML = ``;
-
-  // myFunction();
 }
+
+// determine winner and looser
 
 function clickCounting() {
   if (monsterHealth < 0) {
     monsterHealth = 0;
-    if (yourHealth <= 10) {
+    if (yourHealth < 0) {
       yourHealth = 0;
       setTimeout(function () {
         alert(`It's a draw`);
@@ -170,17 +222,62 @@ function clickCounting() {
       attackButton.disabled = true;
     } else {
       setTimeout(function () {
-        alert("YOU won");
+        // alert("YOU won");
+        playerWon.style.display = "block";
+        playerWon.style.display = "flex";
+        playerWon.style.justifyContent = "flex-start";
+
+        playerPoints++;
+        playerPoint.innerHTML = playerPoints;
+        if (playerPoint.innerHTML == 5) {
+          alert("GAME IS OVER. YOU WON");
+
+          gameOverPoints();
+        }
+
         startAgain();
       }, 1000);
+
       attackButton.disabled = true;
     }
   } else if (yourHealth < 0) {
     yourHealth = 0;
     setTimeout(function () {
-      alert("MONSTER won");
+      // alert("MONSTER won");
+      monsterWon.style.display = "block";
+      monsterWon.style.display = "flex";
+      monsterWon.style.justifyContent = "flex-start";
+
+      monsterPoints++;
+      monsterPoint.innerHTML = monsterPoints;
+      if (monsterPoint.innerHTML == 5) {
+        alert("GAME IS OVER. MONSTER WON");
+        gameOverPoints();
+      }
       startAgain();
     }, 1000);
+
     attackButton.disabled = true;
   }
 }
+
+function gameOverPoints() {
+  monsterPoints = 0;
+  monsterPoint.innerHTML = monsterPoints;
+  playerPoints = 0;
+  playerPoint.innerHTML = playerPoints;
+}
+
+// listNum = 0;
+// function removeListElements() {
+//   listNum++;
+//   const playerLi = document.querySelector(".list li:nth-child(1)")
+//   const monsterLi = document.querySelector(".list li:nth-child(2)");
+//   if (playerLi) {
+//     playerLi.remove();
+//   }
+
+//   if (monsterLi) {
+//     monsterLi.remove();
+//   }
+// }
